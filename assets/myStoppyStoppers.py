b9721c1194fb1227793c25307601795a21e14880
Amir <amir.datasci.2021@gmail.com>
2021-05-14 03:01:08 -0400

    push to main branch

---
+++ assets/myStoppyStoppers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+expl_list=['shit','shits','shitty','bullshit','shi','fucker', 'fuckers','fuck','fck','fk','fuckin','fucking','fawk','fawkin','fawks',
+           'fawking','bitch','bitches','bich','biches','bitchez','bitchz','bitchass','bitch-ass','mother-fucker',
+           'mother-fuckers','motherfucker','motherfuckers','motherfucka','motherfuckas','muthafucka','muthafuckas',
+           'muthafuckaz','motherfuckin','muthafuckin','mothafucka','mothafuckas','dick','dicks','cock','cocks','pussy','cunt','cunts','nigga',
+           'niggas','niggaz','nikka','nikkas','niggah','niggahs','nigerina','whore','whores','hoe','hoes','hoez','assez','asses',
+           'ass','asshole','azz']
+extra_stop_words=['ayyy','ayy','ohh','ooh','aaah','aah','im','know','dont','got','uh','like','yeah','aint','just','cause','want','thats','make','let','ya','yeah','yo','ay','wanna', 'huh','dmx','cardi','missy','2pac','kak','rak','pac','man','em','say','time','come','tell','right','need','way','feel','think','said','youre','look','gotta','day','gon','hit','better','baby','good','new','oh','bout','mind','did','do','ive','aaaaaaaaaaaaahhhhhhhhhhhh','aaaah']
+
+
